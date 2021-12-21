873012db9c9f4344ae5846a74c08c4a0dfce84df
ariscybertech <ariscybertech@gmail.com>
2021-12-21 18:14:59 +0500

    update

--- __/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-  "apiKey": "AIzaSyBFBlU6aB0QxbWo9yMpkxo16YFUI50aldE",
-  "authDomain": "victor-aremu.firebaseapp.com",
-  "databaseURL": "https://victor-aremu.firebaseio.com",
-  "messagingSenderId": "825415542794",
-  "projectId": "victor-aremu",
-  "storageBucket": "victor-aremu.appspot.com"
-});
